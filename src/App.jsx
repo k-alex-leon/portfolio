82a409b72511eb4ef9d25f9a9517db66f9f33bca
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-07 22:31:56 -0500

    feat: three js stats fps instance

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Suspense } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Scene from "./Scene";
-import { Environment } from "@react-three/drei";
-
+import { Environment, Stats } from "@react-three/drei";
 
 function App() {
-
   return (
     <div className="w-full h-full absolute">
       <Canvas>
+        <Stats />
         <ambientLight />
         <directionalLight color="red" intensity={10} />
         <Suspense fallback={null}>
