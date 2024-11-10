eaafd31be6985ac40dc8f611af7492be9d7a88c9
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-10 11:10:48 -0500

    style: text color change to white

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -13,7 +13,7 @@ export default function NotFound() {
         </p>
         <Link
           to="/"
-          className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-3xl font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+          className="inline-flex text-white items-center justify-center rounded-md bg-primary px-4 py-2 text-3xl font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
         >
           <GoHomeFill className="m-4"/>
           Go to Homepage
