596da471fc5cc47eab2217967f3a73ad53de9224
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-10 11:13:05 -0500

    style: display animation, scale up window

--- src/pages/WindowView.jsx
+++ src/pages/WindowView.jsx
@@ -1,7 +1,23 @@
-export default function WindowView({children}){
-    return <>
-    <div className="w-full h-full rounded-[90px]">
-        {children}
-    </div>
+import { easeInOut, motion } from "framer-motion";
+
+export default function WindowView({ children, url }) {
+  return (
+    <>
+      <motion.div
+        className="w-full h-full rounded-[90px]"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.5, ease: easeInOut }}
+      >
+        {url ? (
+          <iframe
+            className="w-full h-full rounded-[90px]"
+            src={url}
+          ></iframe>
+        ) : (
+          children
+        )}
+      </motion.div>
     </>
+  );
 }
