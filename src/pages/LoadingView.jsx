e6efbec9c62dd645bdef94f455554e63f4000df8
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-14 20:16:58 -0500

    style: loader view style width

--- src/pages/LoadingView.jsx
+++ src/pages/LoadingView.jsx
@@ -7,16 +7,21 @@ export default function LoadingView() {
   if (!active) return;
   return (
     <motion.div
-      className="absolute left-0 right-0 w-2/3 h-full mx-auto p-4 flex flex-col items-center justify-center"
+      className="absolute left-0 right-0 w-full h-full mx-auto p-4 flex flex-col items-center justify-center z-20 bg-slate-100"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <h2 className="text-2xl font-bold mb-4 text-center">Loading...</h2>
-      <div className="w-full h-6 bg-gray-200 rounded-full overflow-hidden">
-        <div className="h-full bg-blue-700" style={{ width: `${progress}%` }} />
+      <div className="w-2/4">
+        <h2 className="text-2xl font-bold mb-4 text-center">Loading...</h2>
+        <div className="w-full h-6 bg-gray-200 rounded-full overflow-hidden">
+          <div
+            className="h-full bg-blue-700"
+            style={{ width: `${progress}%` }}
+          />
+        </div>
+        <p className="mt-2 text-center">{`${Math.floor(progress)}%`}</p>
       </div>
-      <p className="mt-2 text-center">{`${Math.floor(progress)}%`}</p>
     </motion.div>
   );
 }
