c8cdaabbbbce77b8b7a9711b454b3a2c2868fba2
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-14 20:14:29 -0500

    style: new fonts (anton and archivo)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'inter-regular' : ['Inter-Regular'],
+        anton : ["Anton", 'sans-serif'],
+      archivo: ["'Archivo Black'"]
+      }
+    },
   },
   plugins: [],
 };
