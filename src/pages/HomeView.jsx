be66e032932862b52c8e7c790cef8e5699253d1c
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-11-25 21:50:59 -0500

    refactor: now the home view contains widgets

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,88 +1,63 @@
-import {
-  FaLinkedin,
-  FaCube,
-  FaGamepad,
-  FaTools,
-  FaGoogle,
-  FaInfo
-} from "react-icons/fa";
 import { BsBing } from "react-icons/bs";
-import { BsAirplaneEnginesFill } from "react-icons/bs";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { MdPhotoSizeSelectActual } from "react-icons/md";
+import { MdPhotoSizeSelectActual, MdWbSunny } from "react-icons/md";
+import { TbTools } from "react-icons/tb";
+import { FaSuitcase } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { MotionConfig, motion } from "framer-motion";
+import { Suspense } from "react";
+import Shapes from "../components/shapes/Shapes";
+
+function WeatherWidget() {
+  return (
+    <MotionConfig>
+      <motion.div className="flex relative col-span-3 rounded-2xl bg-gradient-to-b from-blue-600 to-cyan-500 p-10 shadow-xl">
+        <div className="w-2/3 h-full flex flex-col justify-around text-gray-200">
+          <span className="text-6xl">20°c</span>
+          <h6 className="text-4xl font-medium">Weather</h6>
+        </div>
+
+        <div className="absolute top-0 left-0 right-0 bottom-0 rounded-2xl overflow-hidden">
+          <Suspense fallback={null}>
+            <Shapes />
+          </Suspense>
+        </div>
+      </motion.div>
+    </MotionConfig>
+  );
+}
 
 export default function HomeView() {
-  const apps = [
-    {
-      name: "Gallery",
-      icon: MdPhotoSizeSelectActual,
-      color: "#fcc300",
-    },
-    {
-      name: "Linkedin",
-      icon: FaLinkedin,
-      color: "#2a52f5",
-    },
-    {
-      name: "Cube",
-      icon: FaCube,
-      color: "#30ddda",
-    },
-    {
-      name: "Tetris",
-      icon: FaGamepad,
-      color: "#de03e5",
-    },
-    {
-      name: "AirPlane",
-      icon: BsAirplaneEnginesFill,
-      color: "#b40803",
-    },
-    {
-      name: "Whatsapp",
-      icon: IoLogoWhatsapp,
-      color: "#01bb64",
-    },
-    {
-      name: "Tools",
-      icon: FaTools,
-      color: "#01bb64",
-    },
-    {
-      name: "Info",
-      icon: FaInfo,
-      color: "#03b0c6",
-    },
-  ];
   return (
     <div className="flex flex-col w-full h-full py-16">
-      {/* widgets */}
-      <div className="flex flex-col h-full p-4 mt-10">
-        <Link to={'/bing'} className="relative w-full h-20">
-          <BsBing className="absolute place-self-center left-6 top-0 bottom-0"/>
-          <input className="w-full h-full rounded-full pl-20 bg-white" placeholder="Search..." type="text" />
+      {/* bing search */}
+      <div className="flex flex-col mx-6 my-6 shadow-xl">
+        <Link to={"/bing"} className="relative w-full h-24">
+          <BsBing className="absolute place-self-center left-6 top-0 bottom-0" />
+          <input
+            className="w-full h-full rounded-xl pl-20 bg-white"
+            placeholder="Search..."
+            type="text"
+          />
         </Link>
       </div>
-      <div className="h-auto flex-grow py-6 px-4 mb-4 grid grid-cols-4 grid-rows-2 gap-14">
-        {apps.map((app, inx) => {
-          return (
-            <Link
-              key={inx}
-              to={`/${app.name.toLowerCase()}`}
-              className={`flex flex-col items-center space-y-2 justify-center hover:scale-110`}
-            >
-              <div
-                className='w-full h-full bg-white/20 p-6 rounded-2xl flex items-center justify-center'
-              >
-                <app.icon className="text-white w-2/3 h-full" />
-              </div>
-              <span className="text-white w-full text-center text-3xl">
-                {app.name}
-              </span>
-            </Link>
-          );
-        })}
+      <div className="h-full py-6 px-4 mb-4 grid grid-cols-5 grid-rows-4 gap-10 text-white">
+        {/* weather widget */}
+        <WeatherWidget />
+
+        {/* tools widget */}
+        <div className="flex flex-col space-y-6 justify-center items-center bg-gradient-to-l from-orange-500 to-red-700 col-span-2 row-span-1 rounded-2xl shadow-xl">
+          <TbTools size={100} />
+          <h6 className="font-medium">Tools</h6>
+        </div>
+
+        {/* work widget */}
+        <div className="relative p-6 col-span-5 bg-gradient-to-tr from-slate-700 to-zinc-900 rounded-2xl shadow-2xl">
+          <div className="w-full left-0 right-0 flex justify-between">
+            <h6 className="font-medium">Works</h6>
+            <FaSuitcase />
+          </div>
+          <hr className="h-1 bg-white mt-4" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Sections/Sections.jsx
+++ src/pages/Sections/Sections.jsx
@@ -6,55 +6,91 @@ import {
   easeOut,
   motion,
 } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Sections.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-export default function Sections({ isLocked = true }) {
-  const [locked, setLocked] = useState(isLocked);
+function Home() {
   const show = {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
     transition: { duration: 1, delay: 2, ease: easeInOut },
   };
+  return (
+    <section className="grid grid-cols-1 w-full h-screen items-center justify-items-center place-content-center">
+      {/* introduction */}
+      <div className="w-4/5 text-center">
+        <motion.h4
+          className="text-3xl text-start font-anton text-gray-700"
+          // style={{filter: 'invert(0%)'}}
+          {...show}
+        >
+          Hi there! I'm
+        </motion.h4>
+        <motion.h1
+          className="text-[150px] lg:text-[200px] font-archivo"
+          initial={{
+            opacity: 0,
+            marginBottom: 100,
+            marginTop: 100,
+            filter: "blur(4px)",
+          }}
+          animate={{
+            opacity: 1,
+            marginBottom: 0,
+            marginTop: 0,
+            filter: "blur(0px)",
+          }}
+          transition={{ duration: 1.5, delay: 1, ease: easeInOut }}
+        >
+          KEVIN LEÓN
+        </motion.h1>
+        <motion.p
+          className="text-right text-2xl mr-16 font-anton text-gray-700"
+          {...show}
+        >
+          And this is my portfolio.
+        </motion.p>
+      </div>
+    </section>
+  );
+}
+
+function Work() {
+  return (
+    <section className="w-full h-screen bg-gradient-to-t from-slate-100 to-white">
+      {/* <p className="w-full text-6xl font-archivo">title for word</p> */}
+    </section>
+  );
+}
+
+export default function Sections({ isLocked }) {
+  const parallaxRef = useRef();
+  // const [locked, setLocked] = useState(isLocked);
+
+  const scroll = (num) => {
+    if (!parallaxRef.current) return;
+    parallaxRef.current.scrollTo(num);
+  };
+
+  useEffect(() => {
+    if (!isLocked) scroll(1);
+  }, [!isLocked]);
 
   return (
-    <div className="absolute w-full h-full">
-      <motion.section className=" grid grid-cols-1 w-full h-screen items-center justify-items-center place-content-center">
-        {/* introduction */}
-        <div className="w-4/5 text-center">
-          <motion.h4
-            className="text-3xl text-start font-anton text-gray-700"
-            // style={{filter: 'invert(0%)'}}
-            {...show}
-          >
-            Hi there! I'm
-          </motion.h4>
-          <motion.h1
-            className="text-[150px] lg:text-[200px] font-archivo"
-            initial={{
-              opacity: 0,
-              marginBottom: 100,
-              marginTop: 100,
-              filter: "blur(4px)",
-            }}
-            animate={{
-              opacity: 1,
-              marginBottom: 0,
-              marginTop: 0,
-              filter: "blur(0px)",
-            }}
-            transition={{ duration: 1.5, delay: 1, ease: easeInOut }}
-          >
-            KEVIN LEÓN
-          </motion.h1>
-          <motion.p
-            className="text-right text-2xl mr-16 font-anton text-gray-700"
-            {...show}
-          >
-            And this is my portfolio.
-          </motion.p>
-        </div>
-      </motion.section>
-    </div>
+    <Parallax
+      ref={parallaxRef}
+      className="w-full h-full"
+      // style={{ top: 0, left: 0 }}
+      pages={2}
+      // horizontal
+    >
+      <ParallaxLayer offset={0} speed={1.5}>
+        <Home />
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={2.5}>
+        <Work />
+      </ParallaxLayer>
+    </Parallax>
   );
 }
