deecfeda2d068618f205a0d1254dccc94e7c1f1a
Kevin Leon <91386910+k-alex-leon@users.noreply.github.com>
2024-12-17 21:03:52 -0500

    feat: global data added

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,8 +1,16 @@
 import { create } from "zustand";
 
 export const useStore = create((set, get) => ({
+  // is the phone hover? we control some camera movements with this
   isPhoneHover: false,
   setPhoneHover: (isHover) => set({ isPhoneHover: isHover }),
+  // here we know if the phone is lock
   isPhoneLocked: true,
-  setPhoneLock : (isLock) => set({isPhoneLocked : isLock})
+  setPhoneLock: (isLock) => set({ isPhoneLocked: isLock }),
+  // ge keep the active section (tools, work, info...)
+  status: "",
+  setPageStatus: (st) => set({ status: st }),
+  // this keeps the description of some mesh hover
+  description : "",
+  setDescription : (data) => set({description: data})
 }));
